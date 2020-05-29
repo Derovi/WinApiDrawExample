f0c1639f9bf73347c80639e9b82726fa9ca69b75
derovi <devderovi@gmail.com>
2020-05-29 15:47:44 +0300

    Added background

--- main.cpp
+++ main.cpp
@@ -18,10 +18,16 @@
 #include "objects/pieobject.h"
 #include "animation.h"
 
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 LRESULT MessagesHandler(
         HWND window_handle, UINT message_code, WPARAM w_param, LPARAM l_param);
 
 HINSTANCE instance_handle;
+
+static HDC hdcBackground = nullptr;
 
 INT WinMain(HINSTANCE instance_handle_arg, HINSTANCE,
             LPSTR /* command_line */, int n_cmd_show) {
@@ -42,11 +48,11 @@ INT WinMain(HINSTANCE instance_handle_arg, HINSTANCE,
             /* dwExStyle =    */ 0,
             /* lpClassName =  */ kClassName,
             /* lpWindowName = */ "WinAPI Example",
-            /* dwStyle =      */ WS_OVERLAPPEDWINDOW,
+            /* dwStyle =      */ WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
             /* X =            */ CW_USEDEFAULT,
             /* Y =            */ CW_USEDEFAULT,
-            /* nWidth =       */ 500,
-            /* nHeight =      */ 500,
+            /* nWidth =       */ 1600,
+            /* nHeight =      */ 1100,
             /* hWndParent =   */ nullptr,
             /* hMenu =        */ nullptr,
             /* hInstance =    */ instance_handle,
@@ -108,6 +114,9 @@ class MultipleLinesPainter {
         switch (message_code) {
             case WM_CREATE: {
                 SetTimer(window_handle, TIMER_ID, 20 /* millis */, NULL);
+                background = (HBITMAP) LoadImage(NULL, "background.bmp", IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+                hdcBackground = CreateCompatibleDC(NULL);
+                SelectObject(hdcBackground, background);
                 break;
             }
             case WM_TIMER: {
@@ -159,23 +168,45 @@ class MultipleLinesPainter {
                 InvalidateRect(window_handle, nullptr, true);
                 break;
             }
+            case WM_ERASEBKGND: {
+                return (LRESULT) 1;
+            }
             case WM_PAINT: {
                 PAINTSTRUCT ps;
 
                 HDC hdc = BeginPaint(window_handle, &ps);
 
-                animation->tick(window_handle, hdc);
+                RECT rcWindow;
+                HDC hdcMem;
+                HBITMAP hbmMem;
+                HANDLE hbmOld;
 
-                SelectObject(hdc, GetStockObject(HOLLOW_BRUSH));
+                GetClientRect(window_handle, &rcWindow);
+                hdcMem = CreateCompatibleDC(hdc);
+                hbmMem = CreateCompatibleBitmap(hdc,
+                                                rcWindow.right - rcWindow.left,
+                                                rcWindow.bottom - rcWindow.top);
+                hbmOld = SelectObject(hdcMem, hbmMem);
+                BitBlt(hdcMem,
+                       rcWindow.left, rcWindow.top,
+                       rcWindow.right - rcWindow.left,
+                       rcWindow.bottom - rcWindow.top,
+                       hdcBackground,
+                       0, 0,
+                       SRCCOPY);
+
+                SelectObject(hdcMem, GetStockObject(HOLLOW_BRUSH));
+
+                animation->tick(window_handle, hdcMem);
 
                 for (auto object : objects) {
-                    object->draw(window_handle, hdc);
+                    object->draw(window_handle, hdcMem);
                 }
 
                 // draw text
 
                 DWORD color = GetSysColor(COLOR_BTNFACE);
-                SetBkColor(hdc, color);
+                SetBkColor(hdcMem, color);
 
                 HFONT hFont = CreateFontW(15, 0, 0, 0, FW_MEDIUM, 0, 0, 0, 0,
                                           0, 0, 1, 0, L"Georgia");
@@ -189,40 +220,53 @@ class MultipleLinesPainter {
                 int height = rect.bottom - rect.top;
 
                 std::wstring text = L"Selected object: " + manager.getName();
-                TextOutW(hdc, width / 2 - 3 * lstrlenW(text.c_str()), 50, text.c_str(),
-                        lstrlenW(text.c_str()));
+                TextOutW(hdcMem, width / 2 - 3 * lstrlenW(text.c_str()), 50, text.c_str(),
+                         lstrlenW(text.c_str()));
 
                 // draw help
 
                 text = L"Use left and right arrows to change drawing object";
-                TextOutW(hdc, 100, height - 280, text.c_str(),
+                TextOutW(hdcMem, 100, height - 280, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use left mouse button to add a point to the drawing object";
-                TextOutW(hdc, 100, height - 240, text.c_str(),
+                TextOutW(hdcMem, 100, height - 240, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use right mouse button to undo object drawing";
-                TextOutW(hdc, 100, height - 200, text.c_str(),
+                TextOutW(hdcMem, 100, height - 200, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use ENTER to finish object drawing";
-                TextOutW(hdc, 100, height - 160, text.c_str(),
+                TextOutW(hdcMem, 100, height - 160, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use ctrl + C to clear all objects from the screen";
-                TextOutW(hdc, 100, height - 120, text.c_str(),
+                TextOutW(hdcMem, 100, height - 120, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use SPACE to start/ stop animation";
-                TextOutW(hdc, 100, height - 80, text.c_str(),
+                TextOutW(hdcMem, 100, height - 80, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 text = L"Use R to restart animation";
-                TextOutW(hdc, 100, height - 40, text.c_str(),
+                TextOutW(hdcMem, 100, height - 40, text.c_str(),
                          lstrlenW(text.c_str()));
 
                 DeleteObject(hFont);
+
+                BitBlt(hdc,
+                       rcWindow.left, rcWindow.top,
+                       rcWindow.right - rcWindow.left,
+                       rcWindow.bottom - rcWindow.top,
+                       hdcMem,
+                       0, 0,
+                       SRCCOPY);
+
+                SelectObject(hdcMem, hbmOld);
+                DeleteObject(hbmMem);
+                DeleteDC(hdcMem);
+
                 EndPaint(window_handle, &ps);
                 break;
             }
@@ -244,8 +288,8 @@ class MultipleLinesPainter {
   private:
     std::vector<GraphicObject*> objects;
     ObjectManager manager;
-    int64_t lastTime;
     Animation* animation;
+    HBITMAP background = NULL;
 
     int TIMER_ID;
 };
